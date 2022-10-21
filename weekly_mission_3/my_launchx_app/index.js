221ff401d6b61365403015e512e5390f95af750e
Mauricio García Pérez <ing.magape.fa@gmail.com>
2022-10-21 07:53:12 -0500

    Se realiza el primer programa de la semana 3

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander')
+
+const mauricio = new MissionCommander('Mauricio');
+console.log(mauricio.name);
+
+/*
+npm test
+npm install --save-dev jest
+*/
